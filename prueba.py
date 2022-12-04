b2bb6fa903aa41010da42ea09d4d021c39ab2548
Oscar-Aguilar-Camara <oscar.aguilarcamara@itsva.edu.mx>
2022-12-03 21:01:44 -0600

    ajustes

--- prueba.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-root = tk.Tk()
-root.config(width=300, height=200)
-root.title("Botón en Tk")
-img_boton = tk.PhotoImage(file="agregar.png")
-boton = ttk.Button(image=img_boton)
-boton.place(x=50, y=50)
-root.mainloop()
